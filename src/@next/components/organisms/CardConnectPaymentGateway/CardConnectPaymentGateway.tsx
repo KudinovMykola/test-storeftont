a6a517d56e1f90bf1c18f0b05db45cceb2d81e8d
Mykola Kudinov <m.kudinov@splinestudio.ua>
2021-02-17 18:38:56 +0200

    Gateway complete

--- src/@next/components/organisms/CardConnectPaymentGateway/CardConnectPaymentGateway.tsx
+++ src/@next/components/organisms/CardConnectPaymentGateway/CardConnectPaymentGateway.tsx
@@ -8,7 +8,6 @@ import { IProps } from "./types";
 
 /**
  * CardConnect payment gateway.
- * TODO replace url values as env var.
  */
 
 const ADD_CC = gql`
@@ -20,9 +19,7 @@ const ADD_CC = gql`
         methodId: $methodId
       }
     ) {
-      cardConnectData {
-        token
-      }
+      token
     }
   }
 `;
@@ -35,16 +32,17 @@ const CardConnectPaymentGateway: React.FC<IProps> = ({
 }: IProps) => {
   const pathPart = "https://boltgw.cardconnect.com:8443/itoke/ajax-tokenizer.html";
   const paramsParts = [
+    "frameborder=10px",
+    "scrolling=no",
     "useexpiry=true",
     "usecvv=true",
-    "css=input%2Cselect%7Bwidth%3A%20522px%3Bpadding%3A%206px%2012px%3Bborder%3A%201px%20solid%20%23E5E7E9%3Bborder-radius%3A%206px%3Boutline%3A%20none%3Bmargin-bottom%3A%2015px%3Bfont-size%3A%2015px%3B%7Dinput%7Bheight%3A%2025px%3B%7Dselect%7Bwidth%3A%20100px%3Bheight%3A%2040px%3B%7Dbody%7Bmargin%3A0%3B%7Dlabel%7Bfont-size%3A%2015px%3Bfont-family%3A%20%22Open%20Sans%22%2C%20Arial%2C%20sans-serif%3Bline-height%3A%2026px%3Bmargin-bottom%3A8px%3Bfont-weight%3A%20bold%3B%7Dbutton%3Afocus%2Cinput%3Afocus%2Ca%3Afocus%2Cselect%3Afocus%7Boutline%3A%20none%20!important%3Bbox-shadow%3A%200%200%200%203px%20lightskyblue%20!important%3B%7D",
+    "cardinputmaxlength=16",
+    "css=input%2Cselect%7Bwidth%3A%20522px%3Bpadding%3A%206px%2012px%3Bborder%3A%201px%20solid%20%23E5E7E9%3Bborder-radius%3A%206px%3Boutline%3A%20none%3Bmargin-bottom%3A%2015px%3Bfont-size%3A%2015px%3B%7Dinput%7Bheight%3A%2025px%3B%7Dselect%7Bwidth%3A%20100px%3Bheight%3A%2040px%3B%7Dbody%7Bmargin%3A0%3B%7Dlabel%7Bfont-size%3A%2015px%3Bfont-family%3A%20%22Open%20Sans%22%2C%20Arial%2C%20sans-serif%3Bline-height%3A%2026px%3Bmargin-bottom%3A8px%3Bfont-weight%3A%20bold%3B%7Dbutton%3Afocus%2Cinput%3Afocus%2Ca%3Afocus%2Cselect%3Afocus%7Boutline%3A%20none%20!important%3Bbox-shadow%3A%200%200%200%203px%20lightskyblue%20!important%3B%7D"
   ];
 
   let ccToken = "";
   let ccExpiry = "";
-
-  // TODO this value must be replaced with value from some input-selector
-  const ccMethod = "VISA";
+  let ccMethod = "";
 
   const getIframeUrl = () => {
     return `${pathPart}?${paramsParts.join("&")}`;
@@ -59,7 +57,6 @@ const CardConnectPaymentGateway: React.FC<IProps> = ({
     window.addEventListener(
       "message",
       event => {
-        // TODO validate refreshedData as obj with non-empty strings message and expiry
         if (event.data !== null) {
           const refreshedData = JSON.parse(event.data);
           putIframeVars(refreshedData);
@@ -71,67 +68,68 @@ const CardConnectPaymentGateway: React.FC<IProps> = ({
     );
   };
 
+
   return (
-    /* TODO 
-    * add selector input after iframe for ccMethod
-    * ensure that the form submits the given after updating and validating the data of the iframe in the function
-    * ensure that the form submits the given after select method id
-    * add error block
-    */
-    <Mutation 
-      mutation={ADD_CC} 
+
+    <Mutation
+      mutation={ADD_CC}
       onCompleted={(data: any) => {
-	// BONUS
-        const response = data.checkoutAddCc.cardConnectData;
 
         // Don't know why but checkoutAddCc can be null without onError-calling
-        if(response !== null){
-          console.log('processPayment:');
-          console.log(response.token);
-	
-	  // TODO uncomment for nextstep
-          //processPayment(response.token);
+        if(data.checkoutAddCc){
+          processPayment(data.checkoutAddCc.token);
         } else {
           console.log('checkoutAddCc or cardConnectData is null. Data:');
           console.log(data);
         }
       }}
       onError={(errors: any) =>{
-        // Backend Errors handling in ccData-processing 
+        // TODO Backend Errors handling in ccData-processing
         console.log(errors);
       }}
       >
       {(addCc: Function) => (
-        <S.Form id={formId} ref={formRef} 
+        <S.Form id={formId} ref={formRef}
           onSubmit={(e: any) => {
-            /* TODO: 
-            * delete preventDefault after finish with this Gateway
-            * clear any logs and comments
-            */
-            
-            console.log('ADD CC START');
-            
-            // TODO: send method id value from some input-selector 
-            addCc({
-              variables: {
-                token: ccToken, 
-                expiry: ccExpiry, 
-                methodId: ccMethod
+            setTimeout(() => {
+              if(!ccMethod){
+                console.log('Please choose payment way');
+              } else if(!ccToken || !ccExpiry) {
+                console.log('Incorrect card data. Try again, please');
+              } else {
+                addCc({
+                  variables: {
+                    token: ccToken,
+                    expiry: ccExpiry,
+                    methodId: ccMethod
+                  }
+                });
               }
-            });
+            }, 500);
           }}>
           <iframe
             title="CardPointeTokenizer"
             id="tokenframe"
             name="tokenframe"
             src={getIframeUrl()}
-            frameBorder="0"
-            scrolling="no"
-            width="80%"
+            frameBorder="10px"
+            scrolling="yes"
+            width="100%"
             height="100%"
             onLoad={handleTokenEvent}
           />
-
+          <S.Label>Payment Network</S.Label>
+          <S.Select
+            onChange={(e) => {ccMethod=e.target.value;}}
+            defaultValue=""
+          >
+            <option value="">--</option>
+            <option value="JCB">JCB</option>
+            <option value="MASTERCARD">MASTERCARD</option>
+            <option value="DISCOVERY">DISCOVERY</option>
+            <option value="AMEX">AMEX</option>
+            <option value="VISA">VISA</option>
+          </S.Select>
         </S.Form>
       )}
     </Mutation>
--- src/@next/components/organisms/CardConnectPaymentGateway/styles.ts
+++ src/@next/components/organisms/CardConnectPaymentGateway/styles.ts
@@ -2,5 +2,26 @@ import { styled } from "@styles";
 
 export const Form = styled.form`
   padding: 15px;
-  height: 250px;
+  margin-bottom: 25px;
+  height: 280px;
+`;
+
+export const Label = styled.div`
+  font-size: 15px;
+  font-family: 'Open Sans' , Arial , sans-serif;
+  line-height: 26px;
+  margin-bottom: 8px;
+  font-weight: bold;
+`;
+
+export const Select = styled.select`;
+  display: inline-block;
+  height: 40px;
+  width: 200px;
+  padding: 6px 12px;
+  border: 1px solid #e5e7e9;
+  border-radius: 6px;
+  outline: none;
+  margin-bottom: 15px;
+  font-size: 15px;
 `;
